1fc23883b8a9f2dc3bc1712b79d0545db0a02649
rar34 <ra.cse34@gmail.com>
2024-08-17 11:47:29 +0600

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             <nav>
                 <h6 className="footer-title">Services</h6>
                 <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
+                <a className="link link-hover">All products</a>
                 <a className="link link-hover">Marketing</a>
                 <a className="link link-hover">Advertisement</a>
             </nav>
